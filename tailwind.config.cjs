4341d302e386183cbb947e7c6f37b4545752f0dd
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-02-09 21:18:44 +0500

    tailwind and daisy ui setup and configuration

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        default: ["Josefin Sans"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
+};
